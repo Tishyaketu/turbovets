792685e58a16842af7c2d175d47d1ba9a0210382
Tishyaketu <Tishyaketu@users.noreply.github.com>
2025-10-18 21:43:26 -0700

    started with Authentication + RBAC

--- libs/data/src/index.ts
+++ libs/data/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/data.module';
+export * from "./lib/dto/login.dto";
+export * from "./lib/dto/auth-response.dto";
+export * from "./lib/dto/user.dto";
+export * from "./lib/dto/role.dto";
